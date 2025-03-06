f4f61d87db50ebc00a07cc09c1eb9d3a6b0030f9
Jacob Blevins <jacob.blevins@gatech.edu>
2025-03-05 22:52:05 -0500

    built state machine for hard-coded pathing to goals. rotations not accurate yet.

--- src/control/src/get_control.cpp
+++ src/control/src/get_control.cpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include <fstream>
+#include <iostream>
 
 using std::placeholders::_1;
 
@@ -15,38 +17,135 @@ public:
 
         rclcpp::QoS qos = rclcpp::QoS(10).best_effort(); // Quality of Service
         subscription_odom = this->create_subscription<nav_msgs::msg::Odometry>(
-            "/odom", qos, std::bind(&control::topic_callback, this, _1)); // _1 means the function will allow one argument
+            "/odom_fixed", qos, std::bind(&control::topic_callback, this, _1)); // _1 means the function will allow one argument
 
         publisher_cmdvel = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     }
 
 private:
+        
+    bool wayPoint0_flag = true;
+    bool wayPoint1_flag = false;
+    bool wayPoint2_flag = false;
+    bool wayPoint3_flag = false;
+    bool wayPoint4_flag = false;
+    bool wayPoint5_flag = false;
+
     void topic_callback(const nav_msgs::msg::Odometry & msg)
     {
-        auto current_dist = msg.pose.pose.position.x;
+        auto current_dist_x = msg.pose.pose.position.x;
+        auto current_dist_y = msg.pose.pose.position.y;
+        auto current_theta_z = msg.pose.pose.orientation.z;
 
         // Log the received message IF THIS IS NOT SHOWING, BE SURE THE TOPIC EXISTS IN THE SPECIFIC TERMINAL
-        RCLCPP_INFO(this->get_logger(), "Dist from start: %f", current_dist);
+        // RCLCPP_INFO(this->get_logger(), "Dist from start: (%f,%f)", current_dist_x, current_dist_y);
         
         // Instantiate a Twist for control command and defining desired distance
         auto twist_msg = geometry_msgs::msg::Twist();
-        float desired_dist = 1.5; // meters
-        float desired_angle = 0; // meters
 
+        float desired_x_1 = 1.5; // meters
+
+        float desired_theta_1 = 0.707; // radians
+
+        float desired_y_2 = 1.4; // meters
+
+        float desired_theta_2 = 1; // radians
+
+        float desired_x_3 = 0; // meters
+        
         // Calculate proportional control command which is a twist around the z axis
-        int Kp_angle = 1;
         float Kp_dist = .1;  
 
-        // twist_msg.angular.z = Kp_angle*msg.angular.z; // angle is held in the x object of msg
-        twist_msg.linear.x = 0.1*(desired_dist - current_dist); // distance is held in the y object of msg
+        // twist_msg.linear.x = Kp_dist*(desired_x - current_dist_x); 
+        // twist_msg.linear.y = Kp_dist*(desired_y - current_dist_y); 
 
-        // Only give a distance command if an object is detected (non-zero angle) and if the object is within 0.75m
-        // if (msg.angular.z == 0 || msg.linear.x > 0.75){
-        //     twist_msg.linear.x = 0;
-        // }
+        // RCLCPP_INFO(this->get_logger(), "Twist: (%f,%f)", twist_msg.linear.x,twist_msg.linear.y);
 
-        RCLCPP_INFO(this->get_logger(), "Twist: %f", twist_msg.linear.x);
+        // Velocity to waypoint 1
+        auto dist_1 = std::abs(desired_x_1 - current_dist_x);
+        auto dist_2_rot = std::abs(desired_theta_1 - current_theta_z);
+        auto dist_2 = std::abs(desired_y_2 - current_dist_y);
+        auto dist_3_rot = std::abs(desired_theta_2 - current_theta_z);
+        auto dist_3 = std::abs(desired_x_3 - current_dist_x);
+        
+        if (wayPoint0_flag)
+        {
+          if (dist_1 < 0.01) 
+          {
+              twist_msg.linear.x = 0;
+              wayPoint1_flag = true;
+              wayPoint0_flag = false;
+          } 
+          else
+          {
+              twist_msg.linear.x = .25;
+              RCLCPP_INFO(this->get_logger(), "%f from waypoint 1", dist_1);
+          }
+        }
 
+        // Rotate to face waypoint 2
+        else if (wayPoint1_flag) 
+        {
+          if (dist_2_rot < 0.01) 
+          {
+              twist_msg.angular.z = 0;
+              wayPoint2_flag = true;
+              wayPoint1_flag = false;
+          } 
+          else
+          {
+              twist_msg.angular.z = .25;
+              RCLCPP_INFO(this->get_logger(), "%f from facing toward waypoint 2", dist_2_rot);
+          }
+        }
+
+        // Velocity to waypoint 2
+        else if (wayPoint2_flag) 
+        {
+          if (dist_2 < 0.1) 
+          {
+            twist_msg.linear.x = 0;
+            wayPoint3_flag = true;
+            wayPoint2_flag = false;
+          } 
+          else
+          {
+            twist_msg.linear.x = .25;
+            RCLCPP_INFO(this->get_logger(), "%f from waypoint 2", dist_2);
+          }
+        }
+
+        // Rotate to face waypoint 3
+        else if (wayPoint3_flag) 
+        {
+          if (dist_3_rot < 0.01) 
+          {
+              twist_msg.angular.z = 0;
+              wayPoint4_flag = true;
+              wayPoint3_flag = false;
+          } 
+          else
+          {
+              twist_msg.angular.z = .25;
+              RCLCPP_INFO(this->get_logger(), "%f from facing toward waypoint 3", dist_3_rot);
+          }
+        }
+
+        // Velocity to waypoint 3
+        else if (wayPoint4_flag) 
+        {
+          if (dist_3 < 0.1) 
+          {
+            twist_msg.linear.x = 0;
+            wayPoint5_flag = true;
+            wayPoint4_flag = false;
+          } 
+          else
+          {
+            twist_msg.linear.x = .25;
+            RCLCPP_INFO(this->get_logger(), "%f from waypoint 3", dist_3);
+          }
+        }
         // Publish the command velocity
         publisher_cmdvel->publish(twist_msg);
     } 
